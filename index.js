9be5a83571d2b04dfa671ce785c3c2c95494c97c
clive <clive.dsouza1010@gmail.com>
2019-10-10 16:44:23 -0500

    config

--- index.js
+++ index.js
@@ -27,5 +27,5 @@ app.get('/', (req, res) => {
 require('./app/routes/note.routes.js')(app);
 
 app.listen(5000, () => {
-  console.log("Server is listening on port 3001");
+  console.log("Server is listening on port 5000");
 });
